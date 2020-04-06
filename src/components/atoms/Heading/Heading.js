90b8bcd5f950d4aa9aa6950d39f009172e6ab350
Michal Pawlaczyk <michalm441@gmail.com>
2020-04-06 23:54:00 +0200

    Update: Added 'small' props

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Heading = styled.h1`
-  font-size: 3.6rem;
-  font-weight: ${({ theme }) => theme.font.weight.medium};
+  font-size: ${({ small }) => (small ? '1.8rem' : '3.6rem')};
+  font-weight: ${({ theme, small }) => (small ? theme.font.weight.bold : theme.font.weight.medium)};
   color: #0d1c2e;
   padding-left: 0.3em;
 `;
